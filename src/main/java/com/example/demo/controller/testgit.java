e07bd214ca3138bb76a96edfcbb13b0838901b3c
DESKTOP-FQO43D2\Home <truonghuong240928@gmail.com>
2024-03-21 16:09:47 +0700

    testgit

--- src/main/java/com/example/demo/controller/testgit.java
+++ src/main/java/com/example/demo/controller/testgit.java
@@ -10,22 +10,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class testgit {
     @Autowired
     private KichCoServiceImpl kichCoService;
-
-//    @GetMapping("/index")
-//    public String hienThi(Model model, HttpSession session){
-////        if (session.getAttribute("Name") != null){
-////            //Nếu đã đăng nhập vào trang index
-////            String username = (String) session.getAttribute("username");
-////            String password = (String) session.getAttribute("password");
-////            NhanVien checkLogin = nhanVienService.login(username,password);
-////            model.addAttribute("nhanvienLogin",checkLogin);
-////            //////
-////        model.addAttribute("listKichCo",kichCoService.getAll());
-////        model.addAttribute("KichCo",new KichCo());
-////        model.addAttribute("view", "/KichCo/index.jsp");
-////        return "index";
-////        }
-//////        //Nếu chưa đăng nhập thì return về trang logina
-////        return "index";
-//    }
 }
